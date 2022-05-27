049fd6104dde747ebe1bb10f0d4ae816138501b4
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-05-27 13:36:46 +0200

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import Home from "./pages/Home/index.js"
+import Home from "./pages/Home/index.js";
+
+// todo: Change footer width for correct display
 
 function App() {
-  return <Home />
+  return <Home />;
 }
 
-export default App
+export default App;
